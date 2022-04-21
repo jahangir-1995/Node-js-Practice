4b368294c059ce957e8eff29157d558237bdb318
jahangir-1995 <jahangir199511@gmail.com>
2022-04-21 14:34:32 +0600

    backend api create and loade api on the UI

---
+++ --steps.js
@@ -0,0 +1,16 @@
+/* 
+    *1. create a folder
+    *2. open open cmd line to that folder
+    *3. npm init
+    *4. npm install express
+    -------------------------
+    For Express js
+    -------------------------
+    *1. require express
+    *2. create app variable
+    *3. declare port
+    *4. set app.get('/')
+    *5. listen port 
+    *6. node index.js
+    *7. check your browser for that port
+*/
